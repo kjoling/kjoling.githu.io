ae315b50714958dfa17a4d496ff11ea5a7870af8
keeanj <jolingkw@gmail.com>
2022-02-13 22:05:07 -0800

    Main page updated, work on showing proj descriptions

---
+++ script.js
@@ -0,0 +1,26 @@
+const projects = document.querySelectorAll('.project');
+// for (let project of projects) {
+//     project.addEventListener('click',
+//         function (e) {
+//             const projectInfo = project.querySelector('p');
+//             projectInfo.classList.toggle('hide');
+//             projectInfo.removeEventListener('click', function (e) {
+
+//             });
+
+//         });
+// }
+
+const hide = function (items) {
+    for (let item of items) {
+        const paragraphInfo = item.querySelector('p');
+        item.addEventListener('click', function (e) {
+            paragraphInfo.classList.toggle('hide');
+            console.dir(paragraphInfo);
+            paragraphInfo.removeEventListener('click', hide);
+        });
+        paragraphInfo.removeEventListener('click', hide);
+    }
+}
+
+hide(projects);
